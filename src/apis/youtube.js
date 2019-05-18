9a1e60b0a22623da07f9fc177df7840e56765c32
ranjan360 <ranjan17@navgurukul.org>
2019-05-18 23:35:21 +0530

    youtube clone using react

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+const KEY = 'AIzaSyBVs4T01BCKrcQugg0nECWuuxNhwDIrPgo'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+
+});
